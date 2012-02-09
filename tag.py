bc99f9d2d1d3ed5abdb9025f1c5333060fa470d0
whtsky <whtsky@vip.qq.com>
2012-02-09 19:22:43 +0800

    。

--- tag.py
+++ tag.py
@@ -7,8 +7,9 @@ from hashlib import md5
 import time
 
 def tagcloud(db,limit=100):
-    tags =  [_ for _ in reversed(sorted(db.settings.find_one({'name':'tag-count'},{'_id':0,'name':0}).items(), key=lambda x: x[1])[:limit])]
-    return ''.join(['<a href="/tag/%s" style="font-size:%spt;">%s</a>' % (x,round(math.log(y,tags[0][1]),1)*14+8,x)) for x,y in tags])
+    tags = [_ for _ in reversed(sorted(db.settings.find_one({'name':'tag-count'},{'_id':0,'name':0}).items(), key=lambda x: x[1])[:limit])]
+    return ''.join(['<a href="/tag/%s" style="font-size:%spt;">%s</a>' % (name,round(math.log(count,tags[0][1]),1)*14+8,name) \
+            for name,count in tags])
 
 class TagViewHandler(BaseHandler):
     def get(self,tagname):
