fa4012018ced7e4de40a2d37ede193f854c01d72
whtsky <whtsky@vip.qq.com>
2012-02-09 19:17:12 +0800

    排序给写反了。。

--- node.py
+++ node.py
@@ -12,10 +12,10 @@ class NodeViewHandler(BaseHandler):
     def get(self,nodeid):
         node=self.db.nodes.find_one({'_id':int(nodeid)})
         try:
-            self.render('node.html',node=node,posts=self.db.posts.find({'node':node['_id']},sort=[('changedtime', 1)]),
+            self.render('node.html',node=node,posts=self.db.posts.find({'node':node['_id']},sort=[('changedtime', -1)]),
                          db=self.db,limit=POST_PER_PAGE,md5=md5,time_span=time_span,tagcloud=tagcloud(self.db,limit=10),p=int(self.get_argument('p')))
         except:
-            self.render('node.html',node=node,posts=self.db.posts.find({'node':node['_id']},sort=[('changedtime', 1)]),
+            self.render('node.html',node=node,posts=self.db.posts.find({'node':node['_id']},sort=[('changedtime', -1)]),
                         db=self.db,limit=POST_PER_PAGE,md5=md5,tagcloud=tagcloud(self.db,limit=10),time_span=time_span,p=1)
 
     def post(self,nodeid):
--- tag.py
+++ tag.py
@@ -9,13 +9,14 @@ import time
 def tagcloud(db,limit=100):
     tags =  [_ for _ in reversed(sorted(db.settings.find_one({'name':'tag-count'},{'_id':0,'name':0}).items(), key=lambda x: x[1])[:limit])]
     return ''.join(['<a href="/tag/%s" style="font-size:%spt;">%s</a>' % (x,round(math.log(y,tags[0][1]),1)*14+8,x)) for x,y in tags])
+
 class TagViewHandler(BaseHandler):
     def get(self,tagname):
         try:
-            self.render('tag.html',tagname=tagname,posts=self.db.posts.find({'tags':tagname},sort=[('changedtime', 1)]),
+            self.render('tag.html',tagname=tagname,posts=self.db.posts.find({'tags':tagname},sort=[('changedtime', -1)]),
                 db=self.db,limit=POST_PER_PAGE,md5=md5,time_span=time_span,p=int(self.get_argument('p')))
         except:
-            self.render('tag.html',tagname=tagname,posts=self.db.posts.find({'tags':tagname},sort=[('changedtime', 1)]),
+            self.render('tag.html',tagname=tagname,posts=self.db.posts.find({'tags':tagname},sort=[('changedtime', -1)]),
                 db=self.db,limit=POST_PER_PAGE,md5=md5,time_span=time_span,p=1)
 
 class TagCloudHandler(BaseHandler):
