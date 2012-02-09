3cedf8c7c6c6ba24869a12567787ab0102e82934
whtsky <whtsky@vip.qq.com>
2012-02-09 14:27:50 +0800

    use node id

--- admin.py
+++ admin.py
@@ -9,6 +9,6 @@ class AdminAddNodeHandler(BaseHandler):
         if self.get_secure_cookie('user') not in ('jybox','whtsky'):
             return 
         nodes = self.db.nodes
-        nodes.insert({'_id':nodes.count(),
+        nodes.insert({'_id':self.db.settings.find_and_modify(update={'$inc':{'node_id':1}}, new=True)['node_id'],
                       'name':nodename,
                       })
