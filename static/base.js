f5ad7c935e3e463866276cf47c607ed30ed19384
jybox <m@jybox.net>
2012-02-10 22:45:16 +0800

    解决发贴按钮不好使

--- static/base.js
+++ static/base.js
@@ -17,3 +17,6 @@ function getCookie(name) {
     var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
     return r ? r[1] : undefined;
 }
+$("#newPost").click(function(){
+  location.href="/topics/add";
+});
