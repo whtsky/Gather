5b236181b8529c89892d68ce4f87e8c67f8b8bb1
whtsky <whtsky@vip.qq.com>
2012-02-12 15:57:19 +0800

    完成

--- common.py
+++ common.py
@@ -7,8 +7,12 @@ import re
 from tornado.escape import xhtml_escape
 
 html_killer = re.compile('<[^>]*>')
-url_replace = re.compile(u'[^\(]((?:HTTP|HTTPS|FTP|ED2K|THUNDER|FLASHGETX|http|https|ftp|ed2k|thunder|flashgetx)://[^\u4e00-\u9fa5]+)[^\)]')
-img_replace = re.compile(u'[^\(]((?:HTTP|HTTPS|http|https://[^\u4e00-\u9fa5]+(?:.jpg|.jpeg|.gif|.png|.JPG|.JPEG|.GIF|.PNG))[^)])')
+url_replace = re.compile(u'[^\(]((?:HTTP|HTTPS|FTP|ED2K|THUNDER|FLASHGETX|http|https|ftp|ed2k|thunder|flashgetx)://[^\u4e00-\u9fa5 ]+)')
+url_replace_2 = re.compile(u'((?:HTTP|HTTPS|FTP|ED2K|THUNDER|FLASHGETX|http|https|ftp|ed2k|thunder|flashgetx)://[^\u4e00-\u9fa5 ]+)[^\)]')
+pure_url = re.compile(u'((?:HTTP|HTTPS|FTP|ED2K|THUNDER|FLASHGETX|http|https|ftp|ed2k|thunder|flashgetx)://[^\u4e00-\u9fa5 ]+)')
+img_replace = re.compile(u'[^\(]((?:HTTP|HTTPS|http|https)://[^\u4e00-\u9fa5 ]+(?:.jpg|.jpeg|.gif|.png|.JPG|.JPEG|.GIF|.PNG))')
+img_replace_2 = re.compile(u'((?:HTTP|HTTPS|http|https)://[^\u4e00-\u9fa5 ]+(?:.jpg|.jpeg|.gif|.png|.JPG|.JPEG|.GIF|.PNG))[^)]')
+pure_img = re.compile(u'((?:HTTP|HTTPS|http|https)://[^\u4e00-\u9fa5 ]+(?:.jpg|.jpeg|.gif|.png|.JPG|.JPEG|.GIF|.PNG))')
 
 md = Markdown(extensions=['fenced_code','smart_strong','tables'])
 
@@ -39,11 +43,32 @@ def md_convert(txt):
     #滤去html标签
     for x in set(html_killer.findall(txt)):
         txt = txt.replace(x,xhtml_escape(x))
+
+    try:
+        t = pure_img.findall(txt)[0]
+        if t == txt:
+            return md.convert(u'![%s](%s)' % (t,t))
+    except:
+        try:
+            t = pure_url.findall(txt)[0]
+            if t == txt:
+                return md.convert(u'[%s](%s)' % (t,t))
+        except:
+            pass
+
+
     for x in set(img_replace.findall(txt)):
         txt = txt.replace(x,u'![%s](%s)' % (x,x))
-    for x in set(url_replace.findall(txt)):
+    for x in set(img_replace_2.findall(txt)):
         txt = txt.replace(x,u'![%s](%s)' % (x,x))
-    return md.convert(txt)
+    for x in set(url_replace.findall(txt)):
+        txt = txt.replace(x,u'[%s](%s)' % (x,x))
+    for x in set(url_replace_2.findall(txt)):
+        txt = txt.replace(x,u'[%s](%s)' % (x,x))
+
+    print txt
+
+    return xhtml_escape(md.convert(txt))
 
 def getvalue(dict,keyname):
     try:
