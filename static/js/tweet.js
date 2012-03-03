73f73809edcbea6435c8e26c393472bbb931f679
whtsky <whtsky@vip.qq.com>
2012-03-03 21:09:38 +0800

    请无视这条commit….

--- static/js/tweet.js
+++ static/js/tweet.js
@@ -2,9 +2,10 @@ $("#tweetsubmit").click(function(){
     if ($("#tweetcontent").val() == "")
         return false;
     $("#tweet").modal('hide');
-    $("#tweetcontent").val("");
     $.post("/twitter/tweet",{_xsrf:$("input[name='_xsrf']").val(),
             tweet:$("#tweetcontent").val()},
-        function(data){},"json");
+        function(data){
+            $("#tweetcontent").val("");
+        },"json");
     return false;
 });
