875240d18c6901d40d541b8d062d2ac158b1358b
whtsky <whtsky@vip.qq.com>
2012-04-08 14:21:07 +0800

    增加回页顶

--- assets/js/base.js
+++ assets/js/base.js
@@ -120,4 +120,35 @@ $(function() {
         e.preventDefault();
         search();
     });
-});
+    var $scroller;
+    var $body = $('body');
+    var $html = $('html');
+    if ($body.scrollTop()) {
+        $scroller = $body;
+    } else if ($html.scrollTop()) {
+        $scroller = $html;
+    } else {
+        $body.scrollTop(1);
+        if ($body.scrollTop()) {
+            $scroller = $body.scrollTop(0);
+        } else {
+            $scroller = $html;
+        }
+    }
+    function scrollTo(top) {
+        $scroller.animate({scrollTop: top < 0 ? 0 : top}, 1000);
+    }
+    $('.backtotop').click(function(ev){
+        var hash = this.hash;
+        if (hash) {
+            var $hash = $(hash);
+            if ($hash.length) {
+                scrollTo($hash.offset().top);
+                ev.preventDefault();
+            }
+        } else {
+            scrollTo(0);
+            ev.preventDefault();
+        }
+    });
+})();
