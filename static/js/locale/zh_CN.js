b43c6b5077d462ec8028197876cf4f4d6f0bcf06
whtsky <whtsky@me.com>
2012-09-21 22:58:50 +0800

    update js.

--- static/js/locale/zh_CN.js
+++ static/js/locale/zh_CN.js
@@ -1,1 +1,1 @@
-jQuery.timeago.settings.strings={prefixAgo:null,prefixFromNow:"从现在开始",suffixAgo:"之前",suffixFromNow:null,seconds:"不到 1 分钟",minute:"大约 1 分钟",minutes:"%d 分钟",hour:"大约 1 小时",hours:"大约 %d 小时",numbers:[],wordSeparator:""};
+jQuery.timeago.settings.strings={prefixAgo:null,prefixFromNow:"从现在开始",suffixAgo:"之前",suffixFromNow:null,seconds:"不到 1 分钟",minute:"大约 1 分钟",minutes:"%d 分钟",hour:"大约 1 小时",hours:"大约 %d 小时",numbers:[],wordSeparator:""},i18n={"Are you sure?":"三思啊少年"};
