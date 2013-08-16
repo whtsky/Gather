906f36e9d43c09bff7965014ff551017fb5a4731
whtsky <whtsky@me.com>
2013-08-16 15:34:48 +0800

    Raven uses ``log_exception`` to capture error after Tornado 3.1

--- handlers/sentry.py
+++ handlers/sentry.py
@@ -1,13 +1,12 @@
-import tornado.web
+from tornado import web
+import logging
 
 from raven.contrib.tornado import SentryMixin as _SentryMixin
 
 
-class RequestHandler(_SentryMixin, tornado.web.RequestHandler):
-    def send_error(self, status_code=500, **kwargs):
-        if status_code == 404:  # Don't log to sentry when 404
-            tornado.web.RequestHandler.send_error(
-                self, status_code, **kwargs
-            )
+class RequestHandler(_SentryMixin, web.RequestHandler):
+    def log_exception(self, typ, value, tb):
+        if isinstance(value, web.HTTPError) and value.status_code == 404:
+            super(RequestHandler, self).log_exception(typ, value, tb)
         else:
-            _SentryMixin.send_error(self, status_code, **kwargs)
+            web.RequestHandler.log_exception(typ, value, tb)
