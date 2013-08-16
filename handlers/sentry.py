1380bb925cf9e047ac3ec35698bae8b42b55ac11
whtsky <whtsky@me.com>
2013-08-16 15:25:58 +0800

    super is broken..

--- handlers/sentry.py
+++ handlers/sentry.py
@@ -6,8 +6,8 @@ from raven.contrib.tornado import SentryMixin as _SentryMixin
 class RequestHandler(_SentryMixin, tornado.web.RequestHandler):
     def send_error(self, status_code=500, **kwargs):
         if status_code == 404:  # Don't log to sentry when 404
-            super(tornado.web.RequestHandler, self).send_error(
-                status_code, **kwargs
+            tornado.web.RequestHandler.send_error(
+                self, status_code, **kwargs
             )
         else:
-            super(_SentryMixin, self).send_error(status_code, **kwargs)
+            _SentryMixin.send_error(self, status_code, **kwargs)
