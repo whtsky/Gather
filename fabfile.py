4a42715b83ad8802514c4f8128fd0a30658ba359
whtsky <whtsky@gmail.com>
2013-12-01 15:18:43 +0800

    drop qiniu

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,10 @@
 from fabric.api import *
 
-env.hosts = ['whtsky@ssh.whouz.com']
+env.hosts = ['whtsky@pbb.whouz.com']
 
 
 def update():
     with cd('~/PBB'):
         run('git pull origin master')
         run('./bin/pip install -r requirements.txt')
-    local("~/Downloads/qiniu/qrsync ~/qiniu_PBB.json")
+
