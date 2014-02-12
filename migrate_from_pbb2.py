fc6529f28701337870633a9ba340d4f27e16b604
whtsky <whtsky@gmail.com>
2014-02-13 01:03:42 +0800

    Update migrate script

--- migrate_from_pbb2.py
+++ migrate_from_pbb2.py
@@ -26,47 +26,54 @@ def timestamp_to_datetime(t):
     return datetime.datetime.fromtimestamp(t)
 
 
-for pbb_member in mongo_database.members.find(sort=[('created', 1)]):
-    account = Account(
-        username=pbb_member["name"],
-        email=pbb_member["email"],
-        website=pbb_member["website"],
-        description=pbb_member["description"],
-        role=role(pbb_member["role"]),
-        created=timestamp_to_datetime(pbb_member["created"])
-    )
-    account.create_password(str(pbb_member["_id"]))
-    db.session.add(account)
-
-for pbb_node in mongo_database.nodes.find():
-    node = Node(
-        name=pbb_node["title"],
-        slug=pbb_node["name"],
-        description=pbb_node["description"]
-    )
-    db.session.add(node)
-
-db.session.commit()
-
-
-for pbb_topic in mongo_database.topics.find(sort=[('last_reply_time', 1)]):
-    topic = Topic(
-        title=pbb_topic["title"],
-        content=pbb_topic["content"],
-        author=Account.query.filter_by(name=pbb_topic["author"]).first(),
-        node=Node.query.filter_by(name=pbb_topic["node"].first()),
-        created=timestamp_to_datetime(pbb_topic["created"]),
-        updated=timestamp_to_datetime(pbb_topic["last_reply_time"])
-    )
-    db.session.add(topic)
-    db.session.commit()
-    for pbb_reply in mongo_database.replies.find({'topic': pbb_topic["_id"]},
-                                                 sort=[('index', 1)]):
-        reply = Reply(
-            content=pbb_reply["content"],
-            author=Account.query.filter_by(name=pbb_reply["author"]).first(),
-            topic=topic.id,
-            created=timestamp_to_datetime(pbb_reply["created"])
+def main():
+    for pbb_member in mongo_database.members.find(sort=[('created', 1)]):
+        account = Account(
+            username=pbb_member["name"],
+            email=pbb_member["email"],
+            website=pbb_member["website"],
+            description=pbb_member["description"],
+            role=role(pbb_member["role"]),
+            created=timestamp_to_datetime(pbb_member["created"]),
+            password="need-to-reset"
         )
-        db.session.add(reply)
+        account.create_password(str(pbb_member["_id"]))
+        db.session.add(account)
+
+    for pbb_node in mongo_database.nodes.find():
+        node = Node(
+            name=pbb_node["title"],
+            slug=pbb_node["name"],
+            description=pbb_node["description"]
+        )
+        db.session.add(node)
+
     db.session.commit()
+
+    for pbb_topic in mongo_database.topics.find(sort=[('last_reply_time', 1)]):
+        topic = Topic(
+            title=pbb_topic["title"],
+            content=pbb_topic["content"],
+            author=Account.query.filter_by(username=pbb_topic["author"]).first(),
+            node=Node.query.filter_by(name=pbb_topic["node"]).first(),
+            created=timestamp_to_datetime(pbb_topic["created"]),
+            updated=timestamp_to_datetime(pbb_topic["last_reply_time"])
+        )
+        db.session.add(topic)
+        db.session.commit()
+        for pbb_reply in mongo_database.replies.find({'topic': pbb_topic["_id"]},
+                                                     sort=[('index', 1)]):
+            reply = Reply(
+                content=pbb_reply["content"],
+                author=Account.query.filter_by(username=pbb_reply["author"]).first(),
+                topic=topic.id,
+                created=timestamp_to_datetime(pbb_reply["created"])
+            )
+            db.session.add(reply)
+        db.session.commit()
+
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+    main()
