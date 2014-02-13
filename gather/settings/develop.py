8e31d8e2cedff30cb548c72f388c0d8dccf84854
whtsky <whtsky@gmail.com>
2014-02-13 16:45:36 +0800

    update setting
    This fix #50

--- gather/settings/develop.py
+++ gather/settings/develop.py
@@ -1,14 +1,8 @@
 # -*- coding:utf-8 -*-
 import os
 
-FORUM_DOMAIN = "127.0.0.1"
-FORUM_URL = "http://%s" % FORUM_DOMAIN
-DEFAULT_MAIL_SENDER = "no-reply@%s" % FORUM_DOMAIN
-
 DEBUG = True
 
-CACHE_TYPE = "memcached"
-CACHE_KEY_PREFIX = "gather_"
 SECRET_KEY = "develop"
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///%s' % os.path.join(
