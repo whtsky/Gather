1a5bfa29a0161ac4fc8097d8aba877b526d7e0ac
whtsky <whtsky@gmail.com>
2014-03-08 19:44:27 +0800

    Add atom feed #57

--- gather/frontend/views.py
+++ gather/frontend/views.py
@@ -1,12 +1,21 @@
 # -*- coding:utf-8 -*-
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, make_response
 from gather.topic.models import Topic
 
-bp = Blueprint("frontend", __name__, url_prefix="/")
+bp = Blueprint("frontend", __name__, url_prefix="")
 
 
 @bp.route("/")
 def index():
     topics = Topic.query.order_by(Topic.updated.desc()).limit(5)
     return render_template("index.html", topics=topics)
+
+
+@bp.route("/feed")
+def feed():
+    topics = Topic.query.order_by(Topic.updated.desc()).limit(15)
+    response = make_response(render_template("feed.xml", topics=topics))
+    response.mimetype = "application/atom+xml"
+    return response
+
