6c833cfae3db4ec922f79b939cd6d4f1cb6d250d
whtsky <whtsky@gmail.com>
2014-07-04 23:53:15 +0800

    Lazy load Topic in frontend.views

--- gather/frontend/views.py
+++ gather/frontend/views.py
@@ -1,19 +1,20 @@
 # -*- coding:utf-8 -*-
 
 from flask import Blueprint, render_template, make_response
-from gather.topic.models import Topic
 
 bp = Blueprint("frontend", __name__, url_prefix="")
 
 
 @bp.route("/")
 def index():
+    from gather.topic.models import Topic
     topics = Topic.query.order_by(Topic.updated.desc()).limit(5)
     return render_template("index.html", topics=topics)
 
 
 @bp.route("/feed")
 def feed():
+    from gather.topic.models import Topic
     topics = Topic.query.order_by(Topic.updated.desc()).limit(15)
     response = make_response(render_template("feed.xml", topics=topics))
     response.mimetype = "application/atom+xml"
