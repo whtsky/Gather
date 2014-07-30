a4ba9f1f8cb1abfc33e2405762950607e4c6fbc6
oranzhang <423794590@qq.com>
2014-07-31 00:06:38 +0800

    修复node

--- Gather/models/user.rb
+++ Gather/models/user.rb
@@ -75,13 +75,6 @@ class User
     newpass
   end
 
-
-
-  # You can define indexes on documents using the index macro:
-  # index :field <, :unique => true>
-
-  # You can create a composite key in mongoid to replace the default id using the key macro:
-  # key :field <, :another_field, :one_more ....>
 end
 class GuestUser
   def guest?
